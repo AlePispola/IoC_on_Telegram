4c7f6c0cf0b2c34cc618bd23599a17333d72a3ad
s297421 <simone.capriolo2002@gmail.com>
2025-12-09 15:48:48 +0100

    delete garbage

--- ML/commands.sh
+++
@@ -1,3 +0,0 @@
-### lancia sto comando sul terminale prima della classificazione
-
-!pip install pymongo iocsearcher transformers torch pandas
--- beforMeeting/ML_PERU/playground-2.mongodb.js
+++
@@ -1,6 +0,0 @@
-// MongoDB Playground
-use("GroupMonitoringRelease");
-
-// Restituisce un array di stringhe univoche (chat_name)
-// SOLO per i documenti dove il campo "language" è "English"
-db.getCollection("groups").distinct("chat_name", { "language": "English" });
